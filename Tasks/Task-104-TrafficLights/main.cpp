124782999d877a29d65527e9d10b7f1104895430
Zayden Ramkissoon <zayden.ramkissoon@students.plymouth.ac.uk>
2023-09-28 15:49:17 +0100

    traffic light edit

--- Tasks/Task-104-TrafficLights/main.cpp
+++ Tasks/Task-104-TrafficLights/main.cpp
@@ -12,17 +12,17 @@ LCD_16X2_DISPLAY lcd;
 int main()
 {
     lcd.puts("RED");
-    wait_us(1000000);
+    wait_us(3000000);
 
     amber = 1;
     lcd.cls();
     lcd.puts("Amber");
-    wait_us(1000000);
+    wait_us(3000000);
 
     green = 1;
     lcd.cls();
     lcd.puts("Green");    
-    wait_us(1000000);
+    wait_us(3000000);
 
     lcd.cls();
     lcd.puts("TASK-104");
