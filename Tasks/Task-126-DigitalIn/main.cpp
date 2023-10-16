9814fd4d2dfe57d49ed9a0c18aee8a1885a518ff
ZaydenRamkissoon <zaydenramkissoon139@gmail.com>
2023-10-16 01:12:26 +0100

    130 is next
    finished everything before 130 except the busin wasnt working. also one challenge around 124 was omitted temporarily.

--- Tasks/Task-126-DigitalIn/main.cpp
+++ Tasks/Task-126-DigitalIn/main.cpp
@@ -2,32 +2,37 @@
 
 DigitalIn ButtonA(PG_0); //Button A
 DigitalOut redLED(PC_2); //Red Traffic 1
+DigitalIn ButtonB(PG_1);
+DigitalOut yellowLED(PC_3);
 
 // main() runs in its own thread in the OS
 int main()
 {
     int btnA;
+    int btnB;
     // Turn OFF the red LED
     redLED = 0;
+    yellowLED = 0;
+
 
     while (true) {
     
         // Wait for the button to be pressed
         do {
-            btnA = ButtonA; //Read button A
-        } while (btnA == 0);
+            (btnA = ButtonA , btnB = ButtonB ); //Read button A
+        } while ( btnA == btnB == 0);
 
 
         //Toggle the red LED
-        redLED = !redLED;
+        yellowLED = !yellowLED;
 
         //Wait for noise to settle
         wait_us(10000);
 
         // Wait for the button to be released
         do {
-            btnA = ButtonA; //Read button A
-        } while (btnA == 1);
+             (btnA = ButtonA , btnB = ButtonB ); //Read button A
+        } while (btnA == btnB ==1);
 
         //Wait for noise to settle
         wait_us(10000);
