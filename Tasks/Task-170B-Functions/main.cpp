efbd348372662d16866a1ff62ca93d98918d0aaf
ZaydenRamkissoon <zaydenramkissoon139@gmail.com>
2023-11-08 00:11:59 +0000

    progress?
    check these back later.

--- Tasks/Task-170B-Functions/main.cpp
+++ Tasks/Task-170B-Functions/main.cpp
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include "uop_msb.h"
 
 // LED Bar Display
@@ -7,65 +8,25 @@ DigitalOut LED_RED_LE(LED_RED_LE_PIN);
 DigitalOut LED_GRN_LE(LED_GRN_LE_PIN);
 DigitalOut LED_BLUE_LE(LED_BLUE_LE_PIN);
 
-void led_init()
-{
-    //Disable the LED outputs
-    LED_BAR_OE = 1;
-
-    //Set data to 0
-    dataBits = 0;
-
-    //Give a little time
-    wait_us(1);
-
-    //Enable all latches
-    LED_RED_LE  = 1;
-    LED_GRN_LE  = 1;
-    LED_BLUE_LE  = 1;
-
-    //Give a little time
-    wait_us(1);
-
-    //Enable all latches
-    LED_RED_LE  = 0;
-    LED_GRN_LE  = 0;
-    LED_BLUE_LE  = 0;
-}
+void setLatch(uint8_t dat, char col);
+void led_init(uint8_t dat, bool enabled);
 
 int main()
 {
     printf("Functions demo\n");
 
-    led_init();
+    led_init(0x00, true);
 
     while (true) {
 
         //Update the red
-        wait_us(1);
-        dataBits = 0xFF;    //Set the 8-bit data pattern
-        wait_us(1);
-        LED_RED_LE  = 1;    //Copy dataBits to red latch outputs
-        wait_us(1);         
-        LED_RED_LE  = 0;    //Ignore inputs (dataBits)
-        wait_us(1);
+        setLatch(0xFF,'r');
 
         //Update the green
-        wait_us(1);
-        dataBits = 0b10101010;
-        wait_us(1);
-        LED_GRN_LE  = 1;
-        wait_us(1);
-        LED_GRN_LE  = 0;
-        wait_us(1);
+        setLatch(0b10101010, 'g');
 
         //Update the blue
-        wait_us(1);
-        dataBits = 0b11001100;
-        wait_us(1);
-        LED_BLUE_LE = 1;
-        wait_us(1);
-        LED_BLUE_LE = 0;
-        wait_us(1);
+        setLatch(0b11001100, 'b');
 
 
         for (unsigned int n=0; n<10; n++) {
@@ -90,3 +51,65 @@ int main()
 
 }
 
+void setLatch(uint8_t dat, char col)
+{
+    //For safety
+    LED_RED_LE = 0;
+    LED_GRN_LE = 0;
+    LED_BLUE_LE = 0;
+
+    //Now follow the sequence
+    wait_us(1);
+    dataBits = dat;    //Set the 8-bit data pattern
+    wait_us(1);
+
+    //Depending on the chosen colour
+    switch (col)
+    {
+        case 'r':
+            LED_RED_LE  = 1;    //Copy dataBits to red latch outputs
+            wait_us(1);         
+            LED_RED_LE  = 0;    //Ignore inputs (dataBits)
+            break;
+        case 'g':
+            LED_GRN_LE  = 1;    //Copy dataBits to red latch outputs
+            wait_us(1);         
+            LED_GRN_LE  = 0;    //Ignore inputs (dataBits)
+            break;
+        case 'b':
+            LED_BLUE_LE  = 1;    //Copy dataBits to red latch outputs
+            wait_us(1);         
+            LED_BLUE_LE  = 0;    //Ignore inputs (dataBits)
+            break;
+    }
+    wait_us(1); 
+}
+
+void led_init(uint8_t dat, bool enabled)
+{
+    //Disable the LED outputs
+    LED_BAR_OE = 1;
+
+    //Set data to 0
+    dataBits = dat;
+
+    //Give a little time
+    wait_us(1);
+
+    //Enable all latches
+    LED_RED_LE  = 1;
+    LED_GRN_LE  = 1;
+    LED_BLUE_LE  = 1;
+
+    //Give a little time
+    wait_us(1);
+
+    //Enable all latches
+    LED_RED_LE  = 0;
+    LED_GRN_LE  = 0;
+    LED_BLUE_LE  = 0;
+
+    if (enabled) {
+        LED_BAR_OE = 0;
+    }
+}
