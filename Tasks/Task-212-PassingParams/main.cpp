71f3057725859000d1d520ae1e02108fa920676c
ZaydenRamkissoon <zaydenramkissoon139@gmail.com>
2023-11-26 15:06:36 +0000

    finished 212 i guess
    i didnt bothtert to do the last part which is by ref but its simple so its fine

--- Tasks/Task-212-PassingParams/main.cpp
+++ Tasks/Task-212-PassingParams/main.cpp
@@ -1,4 +1,5 @@
 #include "mbed.h"
+#include <cstdio>
 
 typedef struct {
   double real;
@@ -27,7 +28,33 @@ ComplexNumber_C complexSubtract(const ComplexNumber_C a, const ComplexNumber_C b
     ComplexNumber_C y = complexAdd(a,c);
     return y;
 }
-
+ComplexNumber_C complexMag(const ComplexNumber_C a){
+    ComplexNumber_C result;
+    result.real = sqrt(a.real*a.real + a.imag*a.imag);
+    result.imag = 0;
+    return result;
+}
+ComplexNumber_C complexMultiply(const ComplexNumber_C a, const ComplexNumber_C b) {
+ComplexNumber_C y;
+y.real = (a.real*b.real)-(a.imag*b.imag);
+y.imag = (a.real*b.imag)+(b.real*a.imag);
+return y;
+}
+ComplexNumber_C complexDivide(const ComplexNumber_C a, const ComplexNumber_C b) {
+ComplexNumber_C e = (complexMultiply(a, complexConjugate(b)));
+ComplexNumber_C y;
+ComplexNumber_C n = complexMag(b);
+double m = sqrt(n.real);
+y.real = e.real/m;
+y.imag = e.imag/m;
+return y;
+}
+ComplexNumber_C complexArgument(const ComplexNumber_C a){
+ComplexNumber_C arg;
+arg.real = atan(a.imag/a.real);
+arg.imag = 0;
+return arg;
+}
 
 void complexDisplay(const char *strName, const ComplexNumber_C u) {
     printf("%s = %f + j%f\n", strName, u.real, u.imag);
@@ -39,9 +66,25 @@ void complexDisplay(const char *strName, const ComplexNumber_C u) {
 int main() {
     printf("\n\nTASK312\n");
 
+    double realp;
+    double imagp;
+    double realq;
+    double imagq;
     //Create instance of a complex number
-    ComplexNumber_C p = {2.0, 3.0};
-    ComplexNumber_C q = {1.0, 1.0};
+    printf("Originally, p = 2 + 3j and q = 1 + j now enter some new values.");
+    printf("Enter Re(p)");
+    scanf("%lf" , &realp);
+    printf("Enter Im(p)");
+    scanf("%lf" , &imagp);
+
+    printf("Enter Re(q)");
+    scanf("%lf" , &realq);
+    printf("Enter Im(q)");
+    scanf("%lf", &imagq);
+
+
+    ComplexNumber_C p = {realp, imagp};
+    ComplexNumber_C q = {realq, imagq};
     complexDisplay("p", p);
     complexDisplay("q", q);
  
@@ -53,7 +96,20 @@ int main() {
     complexDisplay("q*", qbar);
     ComplexNumber_C difference = complexSubtract(p, q);
     complexDisplay("p-q", difference);
-
+    ComplexNumber_C magp = complexMag(p);
+    ComplexNumber_C magq = complexMag(q);
+    complexDisplay("|p|", magp);
+    complexDisplay("|q|", magq);
+    ComplexNumber_C product = complexMultiply(p, q);
+    complexDisplay("pxq", product);
+    ComplexNumber_C quotientponq = complexDivide(p,q);
+    complexDisplay("p/q", quotientponq);
+    ComplexNumber_C quotientqonp = complexDivide(q, p);
+    complexDisplay("q/p", quotientqonp);
+    ComplexNumber_C argp = complexArgument(p);
+    complexDisplay("arg(p)", argp);
+    ComplexNumber_C argq = complexArgument(q);
+    complexDisplay("arg(q)", argq);
     
     while (true) {
     }
